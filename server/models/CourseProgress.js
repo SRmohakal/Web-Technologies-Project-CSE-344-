57219f72b5f471617db12e534633731e0959a09a
Md Ataur Rahman <ataurcsesust@gmail.com>
2025-12-04 19:03:00 +0600

    ProgressFinal

--- server/models/CourseProgress.js
+++ server/models/CourseProgress.js
@@ -6,3 +6,5 @@ const courseProgressSchema = new mongoose.Schema({
     completed: {type: Boolean, default: false},
     lectureCompleted: []
 }, {minimize: false})
+
+export const CourseProgress = mongoose.model('CourseProgress', courseProgressSchema);
